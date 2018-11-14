6d50517811964d6bfa1024404f41c13faaca2dc2
Remus Tumac <rtumac@smu.edu>
2018-11-14 16:44:26 -0600

    fixed broken plug_cowboy dep

--- bacongraph/mix.exs
+++ bacongraph/mix.exs
@@ -37,7 +37,8 @@ defmodule Bacongraph.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:plug_cowboy, "> 0.0.0"}
     ]
   end
 end
