53922b860c975bc7ce0698bbc2361849a987c71e
Remus Tumac <rtumac@smu.edu>
2018-12-01 11:01:05 -0600

    added tests for Iterator

--- iterator/test/iterator_test.exs
+++ iterator/test/iterator_test.exs
@@ -2,7 +2,22 @@ defmodule IteratorTest do
   use ExUnit.Case
   doctest Iterator
 
-  test "greets the world" do
-    assert Iterator.hello() == :world
+  test "empty list" do
+    { :ok, pid } = Iterator.start []
+    assert Iterator.next(pid) == nil
+  end
+
+  test "list with one element" do
+    { :ok, pid } = Iterator.start [5]
+    assert Iterator.next(pid) == 5
+    assert Iterator.next(pid) == nil
+  end
+
+  test "subsequent calls to next" do
+    { :ok, pid } = Iterator.start [3, 7, 8]
+    assert Iterator.next(pid) == 3
+    assert Iterator.next(pid) == 7
+    assert Iterator.next(pid) == 8
+    assert Iterator.next(pid) == nil
   end
 end
