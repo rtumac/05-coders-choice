b1d5a030c2f4668200c5ef630969b83dc45d2e4f
Remus Tumac <rtumac@smu.edu>
2018-12-01 10:46:10 -0600

    turned Iterator module into a GenServer

--- iterator/lib/iterator.ex
+++ iterator/lib/iterator.ex
@@ -1,18 +1,27 @@
+# ADD DEFDELAGATE
+
 defmodule Iterator do
-  @moduledoc """
-  Documentation for Iterator.
-  """
+  use GenServer
 
-  @doc """
-  Hello world.
+  #  API  #
 
-  ## Examples
-
-      iex> Iterator.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def start(args \\ []) do
+    GenServer.start(__MODULE__, args)
   end
+
+  def next(server) do
+    GenServer.call(server, :next)
+  end
+
+  #  Server Implementation  #
+
+  def init(args) do
+    { :ok, args }
+  end
+
+  def handle_call(:next, _from, state) do
+    { response, new_state } = state |> List.pop_at(0)
+    { :reply, response, new_state }
+  end
+
 end
