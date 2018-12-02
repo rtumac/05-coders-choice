2f1051ba4d335f90880cbcb3831c184b43f45b7c
Remus Tumac <rtumac@smu.edu>
2018-12-01 23:50:04 -0600

    added tests to search module

--- graph_init/lib/graph_init/state.ex
+++ graph_init/lib/graph_init/state.ex
@@ -2,6 +2,12 @@ defmodule GraphInit.State do
 
   defstruct(
     adjacency_list: %{      #adjacency list representing all the connections in the graph
+      # Bacon:   [:Hoffman, :Pollak],
+      # Pollak:  [:Bacon, :Hoffman],
+      # Hoffman: [:Bacon, :Pollak, :Foster],
+      # Foster:  [:Hoffman]
+
+
       Malkovich: [:Hoffman],
       Hoffman:   [:Malkovich, :Bacon, :Foster, :Pollak],
       Bacon:     [:Hoffman, :Pollak, :Welker, :Stone],
--- search/test/search_test.exs
+++ search/test/search_test.exs
@@ -9,4 +9,36 @@ defmodule SearchTest do
   test "search path within 1 hop" do
     assert Search.get_shortest_path(:Bacon, :Stone) == [:Bacon, :Stone]
   end
+
+  test "2" do
+    assert Search.get_shortest_path(:Bacon, :Foster) == [:Bacon, :Hoffman, :Foster]
+  end
+
+  test "3" do
+    assert Search.get_shortest_path(:Jaoui, :Foster) == [:Jaoui, :Wilson, :Stone, :Bacon, :Hoffman, :Foster]
+  end
+
+  # Exhaustive Test #
+
+  test "exhaustive test" do
+    assert Search.get_shortest_path(:Bacon, :Hoffman) == [:Bacon, :Hoffman]
+    assert Search.get_shortest_path(:Bacon, :Pollak) == [:Bacon, :Pollak]
+    assert Search.get_shortest_path(:Bacon, :Foster) == [:Bacon, :Hoffman, :Foster]
+
+    assert Search.get_shortest_path(:Hoffman, :Bacon) == [:Hoffman, :Bacon]
+    assert Search.get_shortest_path(:Hoffman, :Pollak) == [:Hoffman, :Pollak]
+    assert Search.get_shortest_path(:Hoffman, :Foster) == [:Hoffman, :Foster]
+
+    assert Search.get_shortest_path(:Pollak, :Bacon) == [:Pollak, :Bacon]
+    assert Search.get_shortest_path(:Pollak, :Hoffman) == [:Pollak, :Hoffman]
+    assert Search.get_shortest_path(:Pollak, :Foster) == [:Pollak, :Hoffman, :Foster]
+
+    assert Search.get_shortest_path(:Foster, :Bacon) == [:Foster, :Hoffman, :Bacon]
+    assert Search.get_shortest_path(:Foster, :Hoffman) == [:Foster, :Hoffman]
+    assert Search.get_shortest_path(:Foster, :Pollak) == [:Foster, :Hoffman, :Pollak]
+  end
+
+  test "destination node not in the graph" do
+    assert Search.get_shortest_path(:Hoffman, :Jack) == []
+  end
 end
