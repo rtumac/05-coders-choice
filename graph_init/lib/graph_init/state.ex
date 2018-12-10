a2b2df65c6557ce4cc358905eefeb03c276c1b4b
Remus Tumac <rtumac@smu.edu>
2018-12-09 20:44:26 -0600

    formatted search module tests

--- graph_init/lib/graph_init/state.ex
+++ graph_init/lib/graph_init/state.ex
@@ -1,13 +1,8 @@
 defmodule GraphInit.State do
 
   defstruct(
-    adjacency_list: %{      #adjacency list representing all the connections in the graph
-      # bacon:   [:hoffman, :pollak],
-      # pollak:  [:bacon, :hoffman],
-      # hoffman: [:bacon, :pollak, :foster],
-      # foster:  [:hoffman]
-
-
+    #adjacency list representing all the connections in the graph
+    adjacency_list: %{
       malkovich: [:hoffman],
       hoffman:   [:malkovich, :bacon, :foster, :pollak],
       bacon:     [:hoffman, :pollak, :welker, :stone],
--- search/test/search_test.exs
+++ search/test/search_test.exs
@@ -3,46 +3,48 @@ defmodule SearchTest do
   doctest Search
 
   test "search path to self" do
-    assert Search.get_shortest_path(:bacon, :bacon) == []
+    assert Search.get_shortest_path(:bacon, :bacon, GraphInit.new_graph) == []
   end
 
   test "search path within 1 hop" do
-    assert Search.get_shortest_path(:bacon, :stone) == [:bacon, :stone]
+    assert Search.get_shortest_path(:bacon, :stone, GraphInit.new_graph) == [:bacon, :stone]
   end
 
-  test "2" do
-    assert Search.get_shortest_path(:bacon, :foster) == [:bacon, :hoffman, :foster]
-  end
-
-  test "3" do
-    assert Search.get_shortest_path(:jaoui, :foster) == [:jaoui, :wilson, :stone, :bacon, :hoffman, :foster]
-  end
-
-  # Exhaustive Test #
-
-  test "exhaustive test" do
-    assert Search.get_shortest_path(:bacon, :hoffman) == [:bacon, :hoffman]
-    assert Search.get_shortest_path(:bacon, :pollak) == [:bacon, :pollak]
-    assert Search.get_shortest_path(:bacon, :foster) == [:bacon, :hoffman, :foster]
-
-    assert Search.get_shortest_path(:hoffman, :bacon) == [:hoffman, :bacon]
-    assert Search.get_shortest_path(:hoffman, :pollak) == [:hoffman, :pollak]
-    assert Search.get_shortest_path(:hoffman, :foster) == [:hoffman, :foster]
-
-    assert Search.get_shortest_path(:pollak, :bacon) == [:pollak, :bacon]
-    assert Search.get_shortest_path(:pollak, :hoffman) == [:pollak, :hoffman]
-    assert Search.get_shortest_path(:pollak, :foster) == [:pollak, :hoffman, :foster]
-
-    assert Search.get_shortest_path(:foster, :bacon) == [:foster, :hoffman, :bacon]
-    assert Search.get_shortest_path(:foster, :hoffman) == [:foster, :hoffman]
-    assert Search.get_shortest_path(:foster, :pollak) == [:foster, :hoffman, :pollak]
+  test "search for the longest path possible" do
+    assert Search.get_shortest_path(:jaoui, :foster, GraphInit.new_graph) == [:jaoui, :wilson, :stone, :bacon, :hoffman, :foster]
   end
 
   test "destination node not in the graph" do
-    assert Search.get_shortest_path(:hoffman, :jack) == []
+    assert Search.get_shortest_path(:hoffman, :jack, GraphInit.new_graph) == []
   end
 
   test "empty origin and destination" do
-    assert Search.get_shortest_path(:"", :"") == []
+    assert Search.get_shortest_path(:"", :"", GraphInit.new_graph) == []
   end
+
+  test "exhaustive test of a small graph" do
+    graph = %{
+      bacon:   [:hoffman, :pollak],
+      pollak:  [:bacon, :hoffman],
+      hoffman: [:bacon, :pollak, :foster],
+      foster:  [:hoffman]
+    }
+
+    assert Search.get_shortest_path(:bacon, :hoffman, graph) == [:bacon, :hoffman]
+    assert Search.get_shortest_path(:bacon, :pollak, graph) == [:bacon, :pollak]
+    assert Search.get_shortest_path(:bacon, :foster, graph) == [:bacon, :hoffman, :foster]
+
+    assert Search.get_shortest_path(:hoffman, :bacon, graph) == [:hoffman, :bacon]
+    assert Search.get_shortest_path(:hoffman, :pollak, graph) == [:hoffman, :pollak]
+    assert Search.get_shortest_path(:hoffman, :foster, graph) == [:hoffman, :foster]
+
+    assert Search.get_shortest_path(:pollak, :bacon, graph) == [:pollak, :bacon]
+    assert Search.get_shortest_path(:pollak, :hoffman, graph) == [:pollak, :hoffman]
+    assert Search.get_shortest_path(:pollak, :foster, graph) == [:pollak, :hoffman, :foster]
+
+    assert Search.get_shortest_path(:foster, :bacon, graph) == [:foster, :hoffman, :bacon]
+    assert Search.get_shortest_path(:foster, :hoffman, graph) == [:foster, :hoffman]
+    assert Search.get_shortest_path(:foster, :pollak, graph) == [:foster, :hoffman, :pollak]
+  end
+
 end
